717c5a5285a269b004c32db55895faef343a5194
“saschakala” <mxsaschakala@gmail.com>
2020-11-22 15:31:34 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const input = document.getElementById('create-task-form');
 
   input.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  let taskElement=document.getElementById("new-task-description");
-  let newTask = taskElement.value;
-  console.log(newTask);
+    let taskElement=document.getElementById("new-task-description");
+    let newTask = taskElement.value;
+    // console.log(newTask);
   
-  let newItem = document.createElement('li');
-  newItem.innerText = newTask;
+    let newItem = document.createElement('li');
+    newItem.innerText = newTask;
 
-  let list=document.getElementById("tasks");
-  list.appendChild(newItem);
-  taskElement.value = '';
+    let list=document.getElementById("tasks");
+    list.appendChild(newItem);
+    taskElement.value = '';
   }
 );
   // your code here
@@ -22,9 +22,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // .innerHTML += newTask;
 
-// - set 'submit' to a variable
-// - grab input and set to variable
-// - add an eventlistener for 'submit' variable
-// - stop from refreshing/sending to a new page
-// - add input to array of to do list
-// - add element to DOM
+
+// - set form to a variable
+// - add an eventlistener for 'submit' action on form
+// - stop from refreshing/sending to a new page with .preventDefault
+// - set input element to a variable
+// - set the value of the input element to a variable
+// - create a new li
+// - add input value to the new li
+// - set list to a variable
+// - append new li to list
+// - reset input element to empty
